a002dc2c573c0eb212420abcf32a13253958b2fa
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-12 17:40:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,21 @@
 class Dog
-  attr_accessor :name
 
-     @@all = []
+	@@all = []
+	attr_accessor :name
 
-     def initialize(name)
-      @name = name
-      @@all << self
-    end
+	def initialize(name)
+		@name = name
+		@@all << self
+	end
 
-     def self.clear_all
-      @@all.clear
-    end
+	def self.clear_all
+		@@all.clear
 
-     def self.all
-      @@all.each do |d|
-        puts d.name
-      end
-    end
-  end
-  
+	end
+
+	def self.all
+		@@all.each do |dog|
+			puts dog.name
+		end
+	end
+end 
